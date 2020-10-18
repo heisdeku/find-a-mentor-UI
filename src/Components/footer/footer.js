04dc883264d6a4c370c66f7859696ed4f73fc44e
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-18 18:13:35 +0100

    footer section fully responsive

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => 
-    <footer className="footer">
-        <div className="container h-50">
+    <footer className="footer pt-4">
+        <div className="container h-50 mt-3">
             <div className="row justify-content-start p-md-4">
-                <div className="footer__social-icons col-sm-6">
-                    <div className="google-icon">
+                <div className="footer__social-icons col-sm-6 col-lg-4 mt-4">
+                    <h6 className="footer__bold">Follow Us</h6>
+                    <div className="google-icon pl-3">
                         <span className="fab fa-google"></span>
                     </div>
-                    <div className="facebook-icon">
+                    <div className="facebook-icon pl-3">
                         <span className="fab fa-facebook"></span>
                     </div>    
-                    <div className="twitter-icon">
+                    <div className="twitter-icon pl-3">
                         <span className="fab fa-twitter"></span>
                     </div>             
                 </div>     
-                <div className="footer__information col-sm-6">
+                <div className="footer__information col-sm-6 col-lg-4 my-4">
+                    <h6 className="footer__bold">Information</h6>
                     <ul>
                     <li>Reviews</li>
                     <li>Privacy Policy</li>
@@ -24,6 +26,7 @@ const Footer = () =>
                     </ul>                    
                 </div>
                 <div className="footer__contact col-lg-4">
+                    <h6 className="footer__bold">Contact Details</h6>
                     <ul>
                     <li>+234567890</li>
                     <li>findamentor@gmail.com</li>
